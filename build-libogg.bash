161a8d06b667c466a7bf5af612d660ab284a660b
Mikkel Krautz <mikkel@krautz.dk>
2012-08-05 00:25:12 +0200

    Add OS X build scripts.

---
+++ build-libogg.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+SHASUM="a900af21b6d7db1c7aa74eb0c39589ed9db991b8"
+curl -O "http://downloads.xiph.org/releases/ogg/libogg-1.3.0.tar.gz"
+if [ "$(shasum libogg-1.3.0.tar.gz | cut -b -40)" != "${SHA1}" ]; then
+	echo libogg checksum mismatch
+fi
+tar -zxf libogg-1.3.0.tar.gz
+cd libogg-1.3.0
+./configure --disable-dependency-tracking --prefix=$MUMBLE_PREFIX --disable-shared --enable-static
+make
+make install
---
+++ build-openssl.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+SHA1="31e6e8bbf1de2f59fbd53382c34214887ccc1318"
+curl -O http://www.openssl.org/source/openssl-1.0.0j.tar.gz
+if [ "$(shasum openssl-1.0.0j.tar.gz | cut -b -40)" != "${SHA1}" ]; then
+	echo openssl checksum mismatch
+fi
+tar -zxf openssl-1.0.0j.tar.gz
+cd openssl-1.0.0j
+./Configure darwin64-x86_64-cc no-shared --prefix=$MUMBLE_PREFIX --openssldir=$MUMBLE_PREFIX/openssl
+make
+make install
