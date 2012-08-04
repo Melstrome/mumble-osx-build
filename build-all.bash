74f73132d74fa54bfea8683066dd909124b9e6c8
Mikkel Krautz <mikkel@krautz.dk>
2012-08-05 00:38:59 +0200

    Update README; add build-all.bash.

---
+++ build-all.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+./build-pkgconfig.bash
+./build-openssl.bash
+./build-libxar.bash
+./build-qt.bash
+./build-qt-icnsicon.bash
+./build-boost.bash
+./build-libogg.bash
+./build-libvorbis.bash
+./build-libflac.bash
+./build-libsndfile.bash
+./build-protobuf.bash
