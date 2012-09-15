f344178ef8dfc2686eb96552b1cbc0a10269ec38
Mikkel Krautz <mikkel@krautz.dk>
2012-09-15 14:25:09 +0200

    Update Boost to 1.51.0.

--- build-boost.bash
+++ build-boost.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
-SHA1="ee06f89ed472cf369573f8acf9819fbc7173344e"
-curl -L -O "http://downloads.sourceforge.net/project/boost/boost/1.50.0/boost_1_50_0.tar.bz2"
-if [ "$(shasum -a 1 boost_1_50_0.tar.bz2 | cut -b -40)" != "${SHA1}" ]; then
+SHA1="52ef06895b97cc9981b8abf1997c375ca79f30c5"
+curl -L -O "http://downloads.sourceforge.net/project/boost/boost/1.51.0/boost_1_51_0.tar.bz2"
+if [ "$(shasum -a 1 boost_1_51_0.tar.bz2 | cut -b -40)" != "${SHA1}" ]; then
 	echo boost checksum mismatch
 	exit
 fi
-tar -jxf boost_1_50_0.tar.bz2
-rm -rf $MUMBLE_PREFIX/include/boost_1_50_0
-cd boost_1_50_0
+tar -jxf boost_1_51_0.tar.bz2
+rm -rf $MUMBLE_PREFIX/include/boost_1_51_0
+cd boost_1_51_0
 patch -p1 < ../patches/boost-1.48.0-nil-fix.patch
 cd ..
-mv boost_1_50_0 $MUMBLE_PREFIX/include/
+mv boost_1_51_0 $MUMBLE_PREFIX/include/
