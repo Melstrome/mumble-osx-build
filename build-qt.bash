d120f17b0cbaf393b2cb1219a0c0dbbe6a0520ab
Mikkel Krautz <mikkel@krautz.dk>
2012-09-16 01:01:43 +0200

    build-qt.bash: build Qt with -no-reduce-exports.

--- build-qt.bash
+++ build-qt.bash
@@ -12,6 +12,6 @@ unset CXXFLAGS
 unset LDFLAGS
 export CFLAGS="-I$MUMBLE_PREFIX/include"
 export CXXFLAGS="-I$MUMBLE_PREFIX/include"
-OPENSSL_LIBS="-L$MUMBLE_PREFIX/lib -lssl -lcrypto" ./configure -platform unsupported/macx-clang -static -no-pch -fast -nomake examples -nomake demos -nomake docs -debug-and-release -arch x86_64 -cocoa -qt-sql-sqlite -no-dbus -no-webkit -no-phonon -no-phonon-backend -no-qt3support -no-multimedia -no-audio-backend -qt-zlib -qt-libtiff -qt-libpng -qt-libmng -qt-libjpeg -openssl-linked -mysql_config no -sdk ${OSX_SDK} -prefix ${MUMBLE_PREFIX}/Qt4.8 -opensource -confirm-license
+OPENSSL_LIBS="-L$MUMBLE_PREFIX/lib -lssl -lcrypto" ./configure -platform unsupported/macx-clang -static -no-reduce-exports -no-pch -fast -nomake examples -nomake demos -nomake docs -debug-and-release -arch x86_64 -cocoa -qt-sql-sqlite -no-dbus -no-webkit -no-phonon -no-phonon-backend -no-qt3support -no-multimedia -no-audio-backend -qt-zlib -qt-libtiff -qt-libpng -qt-libmng -qt-libjpeg -openssl-linked -mysql_config no -sdk ${OSX_SDK} -prefix ${MUMBLE_PREFIX}/Qt4.8 -opensource -confirm-license
 make
 make install
