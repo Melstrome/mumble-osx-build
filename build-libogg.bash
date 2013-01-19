a4225cf3262989dc27b2c408dd7fa6776a0833ce
Mikkel Krautz <mikkel@krautz.dk>
2013-01-19 15:10:01 +0100

    build-libogg.bash: fix sha1 checking preventing Ogg support to be included in the build.

--- build-libogg.bash
+++ build-libogg.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-SHASUM="a900af21b6d7db1c7aa74eb0c39589ed9db991b8"
+SHA1="a900af21b6d7db1c7aa74eb0c39589ed9db991b8"
 curl -O "http://downloads.xiph.org/releases/ogg/libogg-1.3.0.tar.gz"
 if [ "$(shasum -a 1 libogg-1.3.0.tar.gz | cut -b -40)" != "${SHA1}" ]; then
 	echo libogg checksum mismatch
