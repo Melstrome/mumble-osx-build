4e857ea11511d33aef5eaf352bb166019f3c0603
Mikkel Krautz <mikkel@krautz.dk>
2013-02-06 00:32:42 +0100

    build-openssl.bash: update to openssl-1.0.0k.

--- build-openssl.bash
+++ build-openssl.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
-SHA1="31e6e8bbf1de2f59fbd53382c34214887ccc1318"
-curl -O http://www.openssl.org/source/openssl-1.0.0j.tar.gz
-if [ "$(shasum -a 1 openssl-1.0.0j.tar.gz | cut -b -40)" != "${SHA1}" ]; then
+SHA1="ec5d20f1ee52ae765b9286e9d7951dcfc9548607"
+curl -O http://www.openssl.org/source/openssl-1.0.0k.tar.gz
+if [ "$(shasum -a 1 openssl-1.0.0k.tar.gz | cut -b -40)" != "${SHA1}" ]; then
 	echo openssl checksum mismatch
 	exit
 fi
-tar -zxf openssl-1.0.0j.tar.gz
-cd openssl-1.0.0j
+tar -zxf openssl-1.0.0k.tar.gz
+cd openssl-1.0.0k
 ./Configure darwin64-x86_64-cc no-shared --prefix=$MUMBLE_PREFIX --openssldir=$MUMBLE_PREFIX/openssl
 make
 make install
